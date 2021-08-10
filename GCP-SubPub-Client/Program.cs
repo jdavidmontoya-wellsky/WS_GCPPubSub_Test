7f03c0baae4af073dc7e9871ae6289f311c11a55
JDavidMont <jesusdavid.montoya@wellsky.com>
2021-08-09 19:30:10 -0500

    Add GCP Sub/Pub solution Test Console

---
+++ GCP-SubPub-Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using Google.Cloud.PubSub.V1;
+using Google.Protobuf;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GCP_SubPub_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flagContinue = true;
+            do
+            {
+                Console.WriteLine("GCP Sub/Pub Test...\n Write message to send(or write exit):");
+                string textSend = Console.ReadLine();
+                if (string.IsNullOrEmpty(textSend) || textSend.ToUpper().Equals("EXIT"))
+                    flagContinue = false;
+                PublishMessageWithCustomAttributesAsync("peak-system-321913", "Consumer_Update", textSend).GetAwaiter().GetResult();
+            } while (flagContinue);
+        }
+
+        public static async Task PublishMessageWithCustomAttributesAsync(string projectId, string topicId, string messageText)
+        {
+            TopicName topicName = TopicName.FromProjectTopic(projectId, topicId);
+            PublisherClient publisher = await PublisherClient.CreateAsync(topicName);
+
+            var pubsubMessage = new PubsubMessage
+            {
+                // The data is any arbitrary ByteString. Here, we're using text.
+                Data = ByteString.CopyFromUtf8(messageText),
+                // The attributes provide metadata in a string-to-string dictionary.
+                Attributes =
+            {
+                { "year", "2020" },
+                { "author", "unknown" }
+            }
+            };
+            string message = await publisher.PublishAsync(pubsubMessage);
+            Console.WriteLine($"Published message {message}");
+        }
+    }
+}
---
+++ GCP-SubPub-ClientConsumer/Program.cs
@@ -0,0 +1,71 @@
+﻿using Google.Cloud.PubSub.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GCP_SubPub_ClientConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GCP Sub/Pub Subscriber Client Test...");
+            PullMessagesWithCustomAttributesAsync("peak-system-321913", "Consumer_Update", false).GetAwaiter().GetResult();
+           // PullMessagesAsync("peak-system-321913", "Consumer_Update", true).GetAwaiter().GetResult();
+            Console.WriteLine("Press INTRO to continue...");
+            Console.ReadLine();
+        }
+
+        public static async Task<int> PullMessagesAsync(string projectId, string subscriptionId, bool acknowledge)
+        {
+            SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+            SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+            // SubscriberClient runs your message handle function on multiple
+            // threads to maximize throughput.
+            int messageCount = 0;
+            Task startTask = subscriber.StartAsync((PubsubMessage message, CancellationToken cancel) =>
+            {
+                string text = System.Text.Encoding.UTF8.GetString(message.Data.ToArray());
+                Console.WriteLine($"Message {message.MessageId}: {text}");
+                Interlocked.Increment(ref messageCount);
+                return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+            });
+            // Run for 5 seconds.
+            await Task.Delay(5000);
+            await subscriber.StopAsync(CancellationToken.None);
+            // Lets make sure that the start task finished successfully after the call to stop.
+            await startTask;
+            return messageCount;
+        }
+
+        public static async Task<List<PubsubMessage>> PullMessagesWithCustomAttributesAsync(string projectId, string subscriptionId, bool acknowledge)
+        {
+            SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+
+            SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+            var messages = new List<PubsubMessage>();
+            Task startTask = subscriber.StartAsync((PubsubMessage message, CancellationToken cancel) =>
+            {
+                messages.Add(message);
+                string text = System.Text.Encoding.UTF8.GetString(message.Data.ToArray());
+                Console.WriteLine($"Message {message.MessageId}: {text}");
+                if (message.Attributes != null)
+                {
+                    foreach (var attribute in message.Attributes)
+                    {
+                        Console.WriteLine($"{attribute.Key} = {attribute.Value}");
+                    }
+                }
+                return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+            });
+            // Run for 7 seconds.
+            await Task.Delay(7000);
+            await subscriber.StopAsync(CancellationToken.None);
+            // Lets make sure that the start task finished successfully after the call to stop.
+            await startTask;
+            return messages;
+        }
+    }
+}
